8ef29346c510bb2c8a3ad5b73253593e14333acf
zuiwuchang <zuiwuchang@gmail.com>
2021-08-13 09:11:45 +0800

    ServeFile

--- m/helper/serve_content.go
+++ m/helper/serve_content.go
@@ -5,9 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"mime"
 	"mime/multipart"
 	"net/http"
 	"net/textproto"
+	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -16,6 +19,29 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
+
+func (h Helper) ServeFile(stream grpc.ServerStream, name string) error {
+	f, e := os.Open(name)
+	if e != nil {
+		return h.ToHTTPError(name, e)
+	}
+	defer f.Close()
+	stat, e := f.Stat()
+	if e != nil {
+		return h.ToHTTPError(name, e)
+	}
+	return h.ServeName(stream, name, stat.ModTime(), f)
+}
+func (h Helper) ServeName(stream grpc.ServerStream, name string,
+	modtime time.Time,
+	content io.ReadSeeker,
+) error {
+	ctype := mime.TypeByExtension(filepath.Ext(name))
+	if ctype == `` {
+		ctype = `application/octet-stream`
+	}
+	return h.ServeContent(stream, ctype, modtime, content)
+}
 
 // ServeContent make stream google.api.HttpBody compatible http download, copy from http.ServeContent.
 func (Helper) ServeContent(stream grpc.ServerStream, contentType string,
